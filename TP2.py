81349e7026ed3bb27d61a8647cb7b33bd7ec3c0d
Elisabeth KJ <elisabeth.kerr-jarrold@polymtl.ca>
2025-02-10 21:11:07 -0500

    4/6 parties done

--- TP2.py
+++ TP2.py
@@ -1,13 +1,13 @@
 """
 TP2 : Gestion d'une base de données d'un hôpital
 
-Groupe de laboratoire : XX
-Numéro d'équipe :  YY
-Noms et matricules : Nom1 (Matricule1), Nom2 (Matricule2)
+Groupe de laboratoire : 02
+Numéro d'équipe :  09
+Noms et matricules : Elisabeth Kerr-Jarrold (2219141), Antoine Pelletier (Matricule2)
 """
 
 import csv
-
+import statistics 
 ########################################################################################################## 
 # PARTIE 1 : Initialisation des données (2 points)
 ##########################################################################################################
@@ -26,10 +26,23 @@ def load_csv(csv_path):
     patients_dict : dictionnaire python (dict)
         Dictionnaire composé des informations contenues dans le fichier csv
     """
-    patients_dict = {}
-
+    
+    
     # TODO : Écrire votre code ici
-
+    file = open(csv_path, 'r')
+    file_read = csv.DictReader(file)
+    
+    patients_dict = {}
+        
+    for row in file_read:
+        patient_name = row['participant_id']  
+        del row['participant_id']
+        patients_dict[patient_name] = row
+    
+    #print(patients_dict['sub-tokyoIngenia04'])
+    #print("\n\n")
+   # print(patients_dict)
+    file.close()
 
     # Fin du code
 
@@ -56,14 +69,20 @@ def load_multiple_csv(csv_path1, csv_path2):
     patients_dict : dictionnaire python (dict)
         Dictionnaire composé des informations contenues dans les deux fichier csv SANS DUPLICATIONS
     """
-    patients_dict = {}
+    
+    patients_dict = load_csv(csv_path1)
+    patients_dict2 = load_csv(csv_path2)
 
-    # TODO : Écrire votre code ici
+    
+    for key, value in patients_dict2.items():
+        if key not in patients_dict:
+            patients_dict[key] = value
 
-
+    #print(patients_dict)
     # Fin du code
 
     return patients_dict
+
 
 ########################################################################################################## 
 # PARTIE 3 : Changements de convention (4 points)
@@ -87,8 +106,11 @@ def update_convention(old_convention_dict):
     new_convention_dict = {}
 
     # TODO : Écrire votre code ici
-
-
+    for key, value in old_convention_dict.items():
+        if 'date_of_scan' in value:
+            value['date_of_scan'] = value['date_of_scan'].replace("-", "\\")
+        
+    ##########ADD NAN33333
     # Fin du code
 
     return new_convention_dict
@@ -117,10 +139,17 @@ def fetch_candidates(patients_dict):
     candidates_list = []
 
     # TODO : Écrire votre code ici
+    for key, value in patients_dict.items():
+        if 'sex' in value:
+            if value['sex'] == 'F':
+                if 'age' in value:
+                    if int(value['age']) >= 25 and int(value['age']) <= 32:
+                        if 'height' in value:
+                            if int(value['height']) > 170:
+                                candidates_list.append(key)
 
 
     # Fin du code
-
     return candidates_list
 
 ########################################################################################################## 
@@ -146,10 +175,33 @@ def fetch_statistics(patients_dict):
             - au troisième niveau: la moyenne et l'écart type --> metrics['M']['age'].keys() == ['mean', 'std'] ...
     
     """
-    metrics = {'M':{}, 'F':{}}
 
     # TODO : Écrire votre code ici
+    metrics = {'M':{}, 'F':{}}
+    metrics['M'] = {
+        'age':{'mean':{}, 'std': {}}, 
+        'height':{'mean':{}, 'std': {}}, 
+        'weight':{'mean':{}, 'std': {}}
+        }
+    metrics['F'] = {
+        'age':{'mean':{}, 'std': {}}, 
+        'height':{'mean':{}, 'std': {}}, 
+        'weight':{'mean':{}, 'std': {}}
+        }
+    
+    age_m = []
+    for key, value in patients_dict.items():
+        if 'sex' in value:
+            if value['sex']=='M':
+                if 'age' in value:
+                    age_m.append(value['sex'])  
 
+
+        if 'age' in value:
+
+    print(metrics)
+
+    
 
     # Fin du code
 
